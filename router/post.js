6f97a181da645b7642da815f97fcc7b4dc517688
menna <menna.mohamed.omar@gmail.com>
2021-06-07 14:43:58 +0200

    add comment

--- router/post.js
+++ router/post.js
@@ -162,6 +162,88 @@ router.delete("/", async (req, res, next) => {
   }
 });
 
+//add comment
+router.post(
+  "/addComment/:id",
+  authenticationMiddleWare,
+  checkRequiredParams(["comment"]),
+  validateRequest([body("comment").isLength({ min: 1 })]),
+  async (req, res, next) => {
+    Post.findById(req.params.id).then((post) => {
+      const newComment = {
+        comment: req.body.comment,
+      };
+      post.comments.unshift(newComment);
+      post.save();
+      res.status(200).send(post);
+    });
+  }
+);
+
+//edit comment
+router.patch(
+  "/editComment/:post_id/:comment_id",
+  authenticationMiddleWare,
+  checkRequiredParams(["comment"]),
+  validateRequest([body("comment").isLength({ min: 1 })]),
+  async (req, res, next) => {
+    try {
+      const id = req.params.post_id;
+      const commid = req.params.comment_id;
+      let post = await Post.findById(id);
+      let comment;
+      await post.comments.find((comm) => {
+        if (comm._id.toString() === commid) {
+          comment = comm;
+        }
+      });
+      console.log(comment)
+      await comment
+        .update({
+          comment: req.body.comment || "empty",
+        })
+        .exec();
+      res.send(comment);
+    } catch (err) {
+      res.status(422).send({
+        error: err,
+        statusCode: 422,
+      });
+    }
+  }
+);
+
+//delete comment
+router.delete(
+  "/deleteComment/:post_id/:comment_id",
+  authenticationMiddleWare,
+  checkRequiredParams(["comment"]),
+  validateRequest([body("comment").isLength({ min: 1 })]),
+  async (req, res, next) => {
+    const id = req.params.post_id;
+    const comment_id = req.params.comment_id;
+    Post.findById(id)
+      .then((post) => {
+        if (
+          post.comments.filter(
+            (comment) => comment._id.toString() === comment_id
+          ).length === 0
+        ) {
+          return res
+            .status(404)
+            .json({ commentnotexists: "Comment does not exist" });
+        }
+        const removeIndex = post.comments
+          .map((item) => item._id.toString())
+          .indexOf(comment_id);
+        post.comments.splice(removeIndex, 1);
+        post.save().then((post) => res.json(post));
+      })
+
+      .catch((err) => res.status(404).json({ postnotfound: "No post found" }));
+  }
+);
+
 /* image */
 const upload = multer({
   limits: {
